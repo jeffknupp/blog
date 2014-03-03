fcc5a9b28c64660b3f3a594836282631dd65fc20
Jeff Knupp <jknupp@appnexus.com>
2014-03-03 17:49:55 -0500

    new

---
+++ in_progress/web.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = ''
+PORT = 8090
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+connection, address = listen_socket.accept()
+request = connection.recv(1024)
+connection.sendall("""HTTP/1.1 200 OK
+Content-type: text/html
+
+
+<html>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>""")
+connection.close()
